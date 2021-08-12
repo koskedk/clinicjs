1a56fb35af5a8dbc72fec14c4c184f59c0fa0dad
Koske Kimutai <koskedk@live.com>
2021-08-12 18:09:36 +0300

    root app initial

---
+++ clinicjs-navbar/src/root-helper.ts
@@ -0,0 +1,18 @@
+export const links=[
+    {
+        "href":"/",
+        "name":"Dashboard"
+    },
+    {
+        "href":"/consultation",
+        "name":"Consultation"
+    },
+    {
+        "href":"/pharmacy",
+        "name":"Pharmacy"
+    },
+    {
+        "href":"/billing",
+        "name":"Billing"
+    }
+]
--- clinicjs-navbar/src/root.component.tsx
+++ clinicjs-navbar/src/root.component.tsx
@@ -1,3 +1,25 @@
+import React, {useEffect, useState} from "react";
+import {links} from "./root-helper";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+
+  const [activelink, setActiveLink] = useState("/");
+
+  useEffect(() => {
+    const current = window.location.pathname.split('/')[1]
+    const getLink = links.filter(x => `/${current.toLowerCase()}` === x.href);
+    if (getLink.length > 0) {
+      setActiveLink(getLink[0].href);
+    }
+  }, []);
+
+  return (
+      <div className="topnav">
+        {links.map((link, index) => {
+          return (
+              <a className={activelink === link.href ? "active" : ""} href={link.href} key={index}>{link.name}</a>
+          );
+        })}
+      </div>
+  );
 }
